e5e7b00c4c48d6ad6c37bda1e87fcd2a32473bb7
rubielito <loquebuscasestaariba@gmail.com>
2026-07-28 22:33:44 -0500

    Add astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+/** @format */
+import { defineConfig } from "astro/config";
+import { settings } from "./src/data/settings";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+export default defineConfig({
+  site: settings.site,
+  integrations: [svelte(), sitemap(), mdx()],
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
+});
