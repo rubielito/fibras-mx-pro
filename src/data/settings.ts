c6baa18a4fd9caa972ce2a7374ac24d0705b2e48
rubielito <loquebuscasestaariba@gmail.com>
2026-07-28 22:37:56 -0500

    Upload src/data/settings.ts

---
+++ src/data/settings.ts
@@ -0,0 +1,10 @@
+/** @format */
+
+export const settings = {
+  site: "https://www.fibrasmx.net",
+  name: "Fibras MX",
+  title: "Fibras MX | Bienvenido a la comunidad fibrera",
+  description:
+    "Fibras MX es una comunidad en donde se comparte todo lo relacionado con el mundo de las FIBRA´s",
+  showLittleSticksPlug: true,
+};
