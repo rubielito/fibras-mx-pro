016429b6a7f752083e00a036fd79fc0f9994e804
rubielito <loquebuscasestaariba@gmail.com>
2026-07-28 22:37:58 -0500

    Upload src/data/socials.ts

---
+++ src/data/socials.ts
@@ -0,0 +1,19 @@
+/** @format */
+
+export const socials = [
+  {
+    name: "Facebook",
+    url: "https://www.facebook.com/FibrasMexico/",
+    icon: "mdi:facebook",
+  },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/fibras-mx/",
+    icon: "mdi:linkedin",
+  },
+  {
+    name: "YouTube",
+    url: "https://www.youtube.com/@fibras-mx/",
+    icon: "mdi:youtube",
+  },
+];
