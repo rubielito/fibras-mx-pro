04a663aaf0be3fda3b7bbf3a9309721fbeace50d
rubielito <loquebuscasestaariba@gmail.com>
2026-07-28 22:38:19 -0500

    Upload src/utils/members.js

---
+++ src/utils/members.js
@@ -0,0 +1,54 @@
+/** @format */
+
+
+
+export const members = [
+  {
+    src: "https://scontent.fpbc2-2.fna.fbcdn.net/v/t39.30808-1/315919285_192780096592537_1895296045907016564_n.jpg?stp=c0.0.200.200a_dst-jpg_p200x200&_nc_cat=100&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeE2vHvO6pL8FhXavNjWDL7J28-9PgCeHb3bz70-AJ4dvQDgd4qtTGXa2IbGVP-dCVBliek-AmZ2Ps-iJrfdGaT3&_nc_ohc=LkyeHmgRWiAAX8BCD7b&_nc_ht=scontent.fpbc2-2.fna&oh=00_AfAzLPDUpjb_SBjMGPol88oG9dfXi4hCShUvfmhZnkTpjg&oe=6413BF9C",
+    alt: "Ivan de la Cruz",
+  },
+  {
+    src: "https://scontent-qro1-2.xx.fbcdn.net/v/t1.6435-9/85238160_3244193388941330_3373942135244980224_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGLIIiTWf65BJL9RzL3J3GDPxuEecG0TOU_G4R5wbRM5eVEIDxUWy5Upv5uNOeY_BVqiyMYvmogGg3q3VoPAFnJ&_nc_ohc=UyOqfzgJPn4AX_p2HFF&_nc_ht=scontent-qro1-2.xx&oh=00_AfBbIwBg1SoWmBbVwCJErrkz_CC-uXxPRgJ7HYgdlAextw&oe=6436F190",
+    alt: "Luis Alberto",
+  },
+  {
+    src: "https://scontent.fpbc2-2.fna.fbcdn.net/v/t39.30808-6/326225968_908394463907114_6199711881509829067_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFknglLhqMXBOOrvq2H8gpmhR2RtfYCKraFHZG19gIqtsmNmVdfvZHxHvUCSkD2PAc1CZlCFxyXQ-8yTSHBCqve&_nc_ohc=gHalOj5XB4wAX9ppYq7&_nc_zt=23&_nc_ht=scontent.fpbc2-2.fna&oh=00_AfCtbToQYiH8SxTx6cu7ycxgkDFXkF3-3RU_x_oA0JSo6A&oe=6413A6C1",
+    alt: "Edgar Raul Bautista",
+  },
+  {
+    src: "https://scontent-qro1-1.xx.fbcdn.net/v/t39.30808-1/280226003_10160707389961874_631422885780176766_n.jpg?stp=dst-jpg_p200x200&_nc_cat=108&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeGaNUQtWnCkSukKIiABzp1Vx-isp6IjGsvH6KynoiMay7qPt1VLc5EejUULZL8JVi2Pev_-DUn03M0JUCGD2ZCs&_nc_ohc=QfArQS0saBEAX_lkm9s&_nc_ht=scontent-qro1-1.xx&oh=00_AfCf0u_FyTVMS1lxO1Fx1uSv5wynesAThKPNZ6skkpe24w&oe=6413A016",
+    alt: "Carlos Calderón",
+  },
+  {
+    src: "https://scontent-qro1-2.xx.fbcdn.net/v/t39.30808-6/327801321_1180716249228663_7278764013117276005_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFpN4uaxmcO9_IDzthNZqWMApwi0Aw_PiACnCLQDD8-IDmkw2Rhi7y54Hmb3a5AisWlowajZe0Q63EPkUpCc2OF&_nc_ohc=FufNYuJSPV0AX9kBleY&_nc_zt=23&_nc_ht=scontent-qro1-2.xx&oh=00_AfB3e91shSPd4LXjLu0PwpT70MEbzw3Kb3vDmZT53TCocA&oe=641381A2",
+    alt: "Falcao Vallejo",
+  },
+  {
+    src: "https://scontent.fpbc2-2.fna.fbcdn.net/v/t39.30808-6/322040041_1315225409056874_3026586744136024750_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFUKVnLyqzGDIqez4vjE05IrfBsgzRZf2yt8GyDNFl_bCM8S1RsDo--QGTSiOK6y865LXQVyvhmHTfYQmmM0azL&_nc_ohc=CDL_hHerLswAX-E7eJz&_nc_zt=23&_nc_ht=scontent.fpbc2-2.fna&oh=00_AfAIlLoX-20EPV1bPvI3JeKDhTHeUx9nelstAWvp81VRBA&oe=6414E4DD",
+    alt: "Juan MAnuel Téllez",
+  },
+  {
+    src: "https://scontent.fpbc2-2.fna.fbcdn.net/v/t39.30808-6/269975605_10222023477559292_4276393069567391685_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFqnvBA2hs4IhsH1-krq6Abyq11_K3LCzDKrXX8rcsLMCrWtFVVuDgh9mpAVzOzUtbBOxpk836bEAGssDElTCjQ&_nc_ohc=1njxTNJOtPgAX964zpR&_nc_zt=23&_nc_ht=scontent.fpbc2-2.fna&oh=00_AfAI0Jvd6-2G7rUnYfjbxQIjjHuVWCP3JT-FQU6i-uYbhw&oe=64138F98",
+    alt: "Alonso del Villar",
+  },
+  {
+    src: "https://scontent.fpbc2-4.fna.fbcdn.net/v/t1.6435-9/81020063_10218165449382600_2075420491979423744_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFMg-bnEPduhYAJcWVkEBbAMAqvLKmiUdswCq8sqaJR2yiTOR4DN89N5TQr_FMoEDBzbZGDysam-hXQNpnVzODL&_nc_ohc=M6fg5DVgBdMAX_TJdDL&_nc_oc=AQlW0W3Rfrk6h4uuGLloTp2B5U5auL08w1eFyRc2wtZEug-3YmpQRzv-t1oGd4QtFFE&_nc_ht=scontent.fpbc2-4.fna&oh=00_AfA_yuhYETvaY6qc_oIKHKm1vH6dozoHTYRRKl7z4CPGIQ&oe=6437055A",
+    alt: "Christian Gutiérrez",
+  },
+  {
+    src: "https://scontent.fpbc2-2.fna.fbcdn.net/v/t1.6435-9/148045161_10215086913565604_8369231554250647081_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHaVZD16N7FgfSY0KHqakhfRAq5ft0TOr9ECrl-3RM6vzMEZx4MusYNehvvjCmmkLeGzjXzpgWNv2wfnFWFggQP&_nc_ohc=MSTn5H2lQ48AX8FiqJ6&_nc_ht=scontent.fpbc2-2.fna&oh=00_AfCyTPu3MDyrzLs6rFF4TH-uKNHUIwcp-8Qbara334HPLg&oe=6436FF5D",
+    alt: "Fernando Casares",
+  },
+  {
+    src: "https://scontent.fpbc2-2.fna.fbcdn.net/v/t39.30808-6/309033692_192355453178230_8713754722398324408_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEpVpPH9bkVG9GwQsCZ8Z5FyXFR8p5PWXnJcVHynk9ZeT2qMLMhabj_aG8eSgOGRzkuvMnmhArStrniGMpaRLoz&_nc_ohc=zDaBqJ-dcScAX_pz4bz&_nc_zt=23&_nc_ht=scontent.fpbc2-2.fna&oh=00_AfAOxsxz5K6rpQ-dsAn5UA1-EFIN2KK0rnQdmnItr73t0A&oe=6414B326",
+    alt: "Renan Ribeiro",
+  },
+  {
+    src: "https://scontent.fpbc2-4.fna.fbcdn.net/v/t39.30808-6/279078064_10160423556793311_1832914144730308344_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHgzcyMr-lfyn4h8JO2x5_VaKXxCKlbwQhopfEIqVvBCDFEdUJIrwWCL9FFjPWil8HVPN_kzTt_EMme-EZfW3fr&_nc_ohc=ai1Ne4i2p4oAX_b0WcP&_nc_zt=23&_nc_ht=scontent.fpbc2-4.fna&oh=00_AfCc-Y_oFldOa2KgSgozflFy_-rDdacylQ7jsSJsDCNN1Q&oe=64141062",
+    alt: "Luis Ortiz de Montellano",
+  },
+  {
+    src: "https://scontent.faca1-1.fna.fbcdn.net/v/t39.30808-6/265061237_661398328564132_8568400684719969638_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGqfydtvQGDDmZqLeJ5_nbwxSDSn1J_Xy3FINKfUn9fLVBx_5cNHEsNLPvjgfMQZYBq_q0Y_vbPnL53HZ4xAe6k&_nc_ohc=oeYKMY_J8GQAX_0gdzR&_nc_ht=scontent.faca1-1.fna&oh=00_AfD2PpXcK9FLKH7e-EnS0ftzN0H3qBUcxQpE8ToNrrJADQ&oe=6431A2FC",
+    alt: "Francisco Cisneros",
+  },
+];
